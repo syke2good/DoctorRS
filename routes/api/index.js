e995626de4dfe6d5edd08f820416560b55b2724a
Eric Zhong <ericzhong@berkeley.edu>
2022-01-07 13:47:49 -0800

    removed unnecessary file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
-const doctorRoutes = require('./doctorRoutes');
 const scheduleRoutes = require('./scheduleRoutes');
 const userRoutes = require('./userRoutes');
 
-router.use('/doctors', doctorRoutes);
 router.use('/schedules', scheduleRoutes);
 router.use('/users', userRoutes);
 
