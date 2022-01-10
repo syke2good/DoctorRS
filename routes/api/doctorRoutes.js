29ac5753e664bb8fac179b457ff5d06245c5a761
Aakash Bhandari <akashbhandari930@gmail.com>
2022-01-10 11:26:31 -0800

    fixed conflict

---
+++ routes/api/doctorRoutes.js
@@ -0,0 +1,83 @@
+const router = require('express').Router();
+const { Doctor } = require('../../models');
+
+router.post('/', async (req, res) => {
+  try {
+    const doctorData = await Doctor.create(req.body);
+
+    req.session.save(() => {
+      req.session.doctor_id = doctorData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(doctorData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const doctorData = await Doctor.findOne({ where: { email: req.body.email } });
+
+    if (!doctorData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await doctorData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.doctor_id = doctorData.id;
+      req.session.logged_in = true;
+
+      res.json({ doctor: doctorData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
+
+
+
+
+
+// const { Appointment } = require('../../models');
+
+// const router = require('express').Router();
+// module.exports = router;
+
+
+
+// router.get('/project/:id', async (req, res) => {
+//     try {
+//       const projectData = await Doctor.findByPk(req.params.id, {
+//         include: [
+//           {
+//             model: Appointment,
+//             attributes: ['name'],
+//           },
+//         ],
+//       });
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-// const doctorRoutes = require('./doctorRoutes');
-const scheduleRoutes = require('./scheduleRoutes');
-const userRoutes = require('./userRoutes');
+const doctorRoutes = require('./doctorRoutes');
+const appointmentRoutes = require('./appointmentRoutes');
 
-// router.use('/doctors', doctorRoutes);
-router.use('/schedules', scheduleRoutes);
-router.use('/users', userRoutes);
+
+router.use('/doctors', doctorRoutes);
+router.use('/schedules', appointmentRoutes);
+
 
 module.exports = router;
+
